88c7eedc5c6dad83bad986856cecdd3d57cf6ed6
GrPugaguga <pugaguga@mail.ru>
2026-03-09 15:53:48 +0500

    fix(lint): add eslint flat config for ESLint v10

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**', 'frontend/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+  prettierConfig,
+];
